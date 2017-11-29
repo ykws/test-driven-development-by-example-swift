f30c779c5fd84cc8737d56453ea16c8bfeebb4df
Yoshiyuki Kawashima <ykawashi7@gmail.com>
2017-11-30 00:58:31 +0900

    Add test Sum times

--- MoneyExampleTests/MoneyExampleTests.swift
+++ MoneyExampleTests/MoneyExampleTests.swift
@@ -97,4 +97,14 @@ class MoneyExampleTests: XCTestCase {
     XCTAssertEqual(Money.dollar(15), result)
   }
 
+  func testSumTimes() {
+    let fiveBucks: Expression = Money.dollar(5)
+    let tenFrancs: Expression = Money.franc(10)
+    let bank: Bank = Bank.init()
+    bank.addRate(from: "CHF", to: "USD", rate: 2)
+    let sum: Expression = Sum.init(fiveBucks, tenFrancs).times(2)
+    let result: Money = bank.reduce(source: sum, to: "USD")
+    XCTAssertEqual(Money.dollar(20), result)
+  }
+
 }
